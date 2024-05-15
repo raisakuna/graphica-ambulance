ec9a99afad031162d61a30ae8c276433d6e53519
sakuna <sakunaxl@gmail.com>
2024-05-15 18:51:34 -0400

    Location-Service added - microservice

--- location/src/main/java/com/vastika/location/controller/LocationController.java
+++ location/src/main/java/com/vastika/location/controller/LocationController.java
@@ -1,8 +1,15 @@
 package com.vastika.location.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping(value="/api/v1")
 public class LocationController {
+
+    @GetMapping(value= "/city")
+    public String getCity(){
+        return "The city of the user is ";
+    }
 }
